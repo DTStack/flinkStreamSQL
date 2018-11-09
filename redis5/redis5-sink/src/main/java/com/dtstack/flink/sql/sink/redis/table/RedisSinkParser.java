bffa0a7b051362190ed9b4aa46bacd9211397fc1
蒋俊杰 <jiangjunjie@jiangjunjiedeMacBook-Pro.local>
2018-11-09 22:06:28 +0800

    add redis sidetable

---
+++ redis5/redis5-sink/src/main/java/com/dtstack/flink/sql/sink/redis/table/RedisSinkParser.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.flink.sql.sink.redis.table;
+
+import com.dtstack.flink.sql.table.AbsTableParser;
+import com.dtstack.flink.sql.table.TableInfo;
+import com.dtstack.flink.sql.util.MathUtil;
+
+import java.util.Map;
+
+public class RedisSinkParser extends AbsTableParser {
+    @Override
+    public TableInfo getTableInfo(String tableName, String fieldsInfo, Map<String, Object> props) {
+        RedisTableInfo redisTableInfo = new RedisTableInfo();
+        parseFieldsInfo(fieldsInfo, redisTableInfo);
+        redisTableInfo.setUrl(MathUtil.getString(props.get(RedisTableInfo.URL_KEY)));
+        redisTableInfo.setDatabase(MathUtil.getString(props.get(RedisTableInfo.DATABASE_KEY)));
+        redisTableInfo.setPassword(MathUtil.getString(props.get(RedisTableInfo.PASSWORD_KEY)));
+        if (props.get(RedisTableInfo.TIMEOUT) != null){
+            redisTableInfo.setTimeout(Integer.parseInt(MathUtil.getString(props.get(RedisTableInfo.TIMEOUT))));
+        }
+        return redisTableInfo;
+    }
+}
---
+++ redis5/redis5-sink/src/main/java/com/dtstack/flink/sql/sink/redis/table/RedisTableInfo.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.flink.sql.sink.redis.table;
+
+import com.dtstack.flink.sql.table.TargetTableInfo;
+import org.apache.flink.calcite.shaded.com.google.common.base.Preconditions;
+
+public class RedisTableInfo extends TargetTableInfo {
+
+    private static final String CURR_TYPE = "redis";
+
+    public static final String URL_KEY = "url";
+
+    public static final String DATABASE_KEY = "database";
+
+    public static final String PASSWORD_KEY = "password";
+
+    public static final String TIMEOUT = "timeout";
+
+    public RedisTableInfo(){
+        setType(CURR_TYPE);
+    }
+
+    private String url;
+
+    private String database;
+
+    private String password;
+
+    private int timeout = 1000;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(int timeout) {
+        this.timeout = timeout;
+    }
+
+    @Override
+    public boolean check() {
+        Preconditions.checkNotNull(url, "redis field of URL is required");
+        Preconditions.checkNotNull(database, "redis field of database is required");
+        Preconditions.checkNotNull(password, "redis field of password is required");
+        return true;
+    }
+
+    @Override
+    public String getType() {
+        return super.getType().toLowerCase();
+    }
+
+}
