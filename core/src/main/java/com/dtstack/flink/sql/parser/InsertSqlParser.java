35869af7c79ae73589b50f6214d51fffce7bc9a1
maqi <313328862@qq.com>
2018-12-12 20:07:34 +0800

    fix conflict

--- core/src/main/java/com/dtstack/flink/sql/parser/InsertSqlParser.java
+++ core/src/main/java/com/dtstack/flink/sql/parser/InsertSqlParser.java
@@ -21,12 +21,7 @@
 package com.dtstack.flink.sql.parser;
 
 import org.apache.calcite.config.Lex;
-import org.apache.calcite.sql.SqlBasicCall;
-import org.apache.calcite.sql.SqlInsert;
-import org.apache.calcite.sql.SqlJoin;
-import org.apache.calcite.sql.SqlKind;
-import org.apache.calcite.sql.SqlNode;
-import org.apache.calcite.sql.SqlSelect;
+import org.apache.calcite.sql.*;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.commons.lang3.StringUtils;
