dcff87d7f28b00b73aef8a18f5e8c2ffba397bce
yanxi0227 <yanxi@dtstack.com>
2019-04-01 14:40:58 +0800

    check BOOTSTRAPSERVERS_KEY

--- kafka09/kafka09-source/src/main/java/com/dtstack/flink/sql/source/kafka/table/KafkaSourceParser.java
+++ kafka09/kafka09-source/src/main/java/com/dtstack/flink/sql/source/kafka/table/KafkaSourceParser.java
@@ -69,7 +69,7 @@ public class KafkaSourceParser extends AbsSourceParser {
         parseFieldsInfo(fieldsInfo, kafka09SourceTableInfo);
 
         kafka09SourceTableInfo.setParallelism(MathUtil.getIntegerVal(props.get(KafkaSourceTableInfo.PARALLELISM_KEY.toLowerCase())));
-        String bootstrapServer = MathUtil.getString(props.get(KafkaSourceTableInfo.GROUPID_KEY.toLowerCase()));
+        String bootstrapServer = MathUtil.getString(props.get(KafkaSourceTableInfo.BOOTSTRAPSERVERS_KEY.toLowerCase()));
         if (bootstrapServer == null || bootstrapServer.trim().equals("")){
             throw new Exception("BootstrapServers can not be empty!");
         } else {
