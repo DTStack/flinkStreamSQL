1ee869eb6ca9dd44a74cca6b5b0d71c1a7fa6ccb
maqi <maqi@dtstack.com>
2019-09-03 10:34:12 +0800

    modify dirtyDataFrequency  name

--- kafka10/kafka10-source/src/main/java/com/dtstack/flink/sql/source/kafka/CustomerJsonDeserialization.java
+++ kafka10/kafka10-source/src/main/java/com/dtstack/flink/sql/source/kafka/CustomerJsonDeserialization.java
@@ -67,7 +67,7 @@ public class CustomerJsonDeserialization extends AbsDeserialization<Row> {
 
     private static final long serialVersionUID = 2385115520960444192L;
 
-    private static int rowLenth = 1000;
+    private static int dirtyDataFrequency = 1000;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -117,7 +117,7 @@ public class CustomerJsonDeserialization extends AbsDeserialization<Row> {
         try {
             JsonNode root = objectMapper.readTree(message);
 
-            if (numInRecord.getCount()%rowLenth == 0){
+            if (numInRecord.getCount() % dirtyDataFrequency == 0) {
                 LOG.info(root.toString());
             }
 
@@ -148,7 +148,7 @@ public class CustomerJsonDeserialization extends AbsDeserialization<Row> {
             return row;
         } catch (Exception e) {
             //add metric of dirty data
-            if (dirtyDataCounter.getCount()%rowLenth == 0){
+            if (dirtyDataCounter.getCount() % dirtyDataFrequency == 0) {
                 LOG.info("dirtyData: " + new String(message));
                 LOG.error(" ", e);
             }
