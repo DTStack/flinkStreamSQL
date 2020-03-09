785811c41652c2499762d2591dc509729b52c333
maqi <maqi@dtstack.com>
2020-03-09 14:01:45 +0800

    parse async side timestamp type

--- core/src/main/java/com/dtstack/flink/sql/util/MathUtil.java
+++ core/src/main/java/com/dtstack/flink/sql/util/MathUtil.java
@@ -16,7 +16,6 @@
  * limitations under the License.
  */
 
- 
 
 package com.dtstack.flink.sql.util;
 
@@ -27,6 +26,16 @@ import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+import java.util.regex.Pattern;
+
+import static java.time.format.DateTimeFormatter.ISO_INSTANT;
+
 /**
  * Convert val to specified numeric type
  * Date: 2017/4/21
@@ -35,6 +44,13 @@ import java.text.SimpleDateFormat;
  */
 
 public class MathUtil {
+
+    private static final Pattern DATETIME = Pattern.compile("^\\d{4}-(?:0[0-9]|1[0-2])-[0-9]{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{3,9})?Z$");
+    private static final Pattern DATE = Pattern.compile("^\\d{4}-(?:0[0-9]|1[0-2])-[0-9]{2}$");
+    private static final SimpleDateFormat TIMESTAMP_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    private static final int MILLIS_PER_SECOND = 1000;
 
     public static Long getLongVal(Object obj) {
         if (obj == null) {
@@ -126,12 +142,12 @@ public class MathUtil {
             return Double.valueOf((String) obj);
         } else if (obj instanceof Float) {
             return ((Float) obj).doubleValue();
-        } else if (obj instanceof Double){
+        } else if (obj instanceof Double) {
             return (Double) obj;
-        }else if (obj instanceof BigDecimal) {
+        } else if (obj instanceof BigDecimal) {
             return ((BigDecimal) obj).doubleValue();
-        }else if (obj instanceof Integer){
-            return ((Integer)obj).doubleValue();
+        } else if (obj instanceof Integer) {
+            return ((Integer) obj).doubleValue();
         }
 
         throw new RuntimeException("not support type of " + obj.getClass() + " convert to Double.");
@@ -229,18 +245,37 @@ public class MathUtil {
             return null;
         }
         if (obj instanceof String) {
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            try {
-                return new Date(format.parse((String) obj).getTime());
-            } catch (ParseException e) {
-                throw new RuntimeException("String convert to Date fail.");
-            }
+            return getDateFromStr((String) obj);
         } else if (obj instanceof Timestamp) {
             return new Date(((Timestamp) obj).getTime());
         } else if (obj instanceof Date) {
             return (Date) obj;
         }
         throw new RuntimeException("not support type of " + obj.getClass() + " convert to Date.");
+    }
+
+
+    public static Date getDateFromStr(String dateStr) {
+        // 2020-01-01 format
+        if (DATE.matcher(dateStr).matches()) {
+            // convert from local date to instant
+            Instant instant = LocalDate.parse(dateStr).atTime(LocalTime.of(0, 0, 0, 0)).toInstant(ZoneOffset.UTC);
+            // calculate the timezone offset in millis
+            int offset = TimeZone.getDefault().getOffset(instant.toEpochMilli());
+            // need to remove the offset since time has no TZ component
+            return new Date(instant.toEpochMilli() - offset);
+        } else if (DATETIME.matcher(dateStr).matches()) {
+            // 2020-01-01T12:12:12Z format
+            Instant instant = Instant.from(ISO_INSTANT.parse(dateStr));
+            return new Date(instant.toEpochMilli());
+        } else {
+            try {
+                // 2020-01-01 12:12:12.0 format
+                return new Date(TIMESTAMP_FORMAT.parse(dateStr).getTime());
+            } catch (ParseException e) {
+                throw new RuntimeException("String convert to Date fail.");
+            }
+        }
     }
 
     public static Timestamp getTimestamp(Object obj) {
@@ -252,8 +287,32 @@ public class MathUtil {
         } else if (obj instanceof Date) {
             return new Timestamp(((Date) obj).getTime());
         } else if (obj instanceof String) {
-            return new Timestamp(getDate(obj).getTime());
+            return getTimestampFromStr(obj.toString());
         }
         throw new RuntimeException("not support type of " + obj.getClass() + " convert to Date.");
     }
+
+    public static Timestamp getTimestampFromStr(String timeStr) {
+        if (DATETIME.matcher(timeStr).matches()) {
+            Instant instant = Instant.from(ISO_INSTANT.parse(timeStr));
+            return new Timestamp(instant.getEpochSecond() * MILLIS_PER_SECOND);
+        } else {
+            Date date = null;
+            try {
+                date = new Date(TIMESTAMP_FORMAT.parse(timeStr).getTime());
+            } catch (ParseException e) {
+                throw new RuntimeException("getTimestampFromStr error data is " + timeStr);
+            }
+            return new Timestamp(date.getTime());
+        }
+    }
+
+    public static String getStringFromTimestamp(Timestamp timestamp) {
+        return TIMESTAMP_FORMAT.format(timestamp);
+    }
+
+    public static String getStringFromDate(Date date) {
+        return DATE_FORMAT.format(date);
+    }
+
 }
---
+++ rdb/rdb-side/src/main/java/io/vertx/core/json/Json.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (c) 2011-2017 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.core.json;
+
+import com.dtstack.flink.sql.util.MathUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import io.netty.buffer.ByteBufInputStream;
+import io.vertx.core.buffer.Buffer;
+
+import java.io.DataInput;
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.time.format.DateTimeFormatter.ISO_INSTANT;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class Json {
+
+    public static ObjectMapper mapper = new ObjectMapper();
+    public static ObjectMapper prettyMapper = new ObjectMapper();
+
+    static {
+        // Non-standard JSON but we allow C style comments in our JSON
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+
+        prettyMapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+        prettyMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+
+        SimpleModule module = new SimpleModule();
+        // custom types
+        module.addSerializer(JsonObject.class, new JsonObjectSerializer());
+        module.addSerializer(JsonArray.class, new JsonArraySerializer());
+        // he have 2 extensions: RFC-7493
+        module.addSerializer(Instant.class, new InstantSerializer());
+        module.addSerializer(byte[].class, new ByteArraySerializer());
+
+        mapper.registerModule(module);
+        prettyMapper.registerModule(module);
+    }
+
+    /**
+     * Encode a POJO to JSON using the underlying Jackson mapper.
+     *
+     * @param obj a POJO
+     * @return a String containing the JSON representation of the given POJO.
+     * @throws EncodeException if a property cannot be encoded.
+     */
+    public static String encode(Object obj) throws EncodeException {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new EncodeException("Failed to encode as JSON: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Encode a POJO to JSON using the underlying Jackson mapper.
+     *
+     * @param obj a POJO
+     * @return a Buffer containing the JSON representation of the given POJO.
+     * @throws EncodeException if a property cannot be encoded.
+     */
+    public static Buffer encodeToBuffer(Object obj) throws EncodeException {
+        try {
+            return Buffer.buffer(mapper.writeValueAsBytes(obj));
+        } catch (Exception e) {
+            throw new EncodeException("Failed to encode as JSON: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Encode a POJO to JSON with pretty indentation, using the underlying Jackson mapper.
+     *
+     * @param obj a POJO
+     * @return a String containing the JSON representation of the given POJO.
+     * @throws EncodeException if a property cannot be encoded.
+     */
+    public static String encodePrettily(Object obj) throws EncodeException {
+        try {
+            return prettyMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new EncodeException("Failed to encode as JSON: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Decode a given JSON string to a POJO of the given class type.
+     * @param str the JSON string.
+     * @param clazz the class to map to.
+     * @param <T> the generic type.
+     * @return an instance of T
+     * @throws DecodeException when there is a parsing or invalid mapping.
+     */
+    public static <T> T decodeValue(String str, Class<T> clazz) throws DecodeException {
+        try {
+            return mapper.readValue(str, clazz);
+        } catch (Exception e) {
+            throw new DecodeException("Failed to decode: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Decode a given JSON string to a POJO of the given type.
+     * @param str the JSON string.
+     * @param type the type to map to.
+     * @param <T> the generic type.
+     * @return an instance of T
+     * @throws DecodeException when there is a parsing or invalid mapping.
+     */
+    public static <T> T decodeValue(String str, TypeReference<T> type) throws DecodeException {
+        try {
+            return mapper.readValue(str, type);
+        } catch (Exception e) {
+            throw new DecodeException("Failed to decode: " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Decode a given JSON buffer to a POJO of the given class type.
+     * @param buf the JSON buffer.
+     * @param type the type to map to.
+     * @param <T> the generic type.
+     * @return an instance of T
+     * @throws DecodeException when there is a parsing or invalid mapping.
+     */
+    public static <T> T decodeValue(Buffer buf, TypeReference<T> type) throws DecodeException {
+        try {
+            return mapper.readValue(new ByteBufInputStream(buf.getByteBuf()), type);
+        } catch (Exception e) {
+            throw new DecodeException("Failed to decode:" + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Decode a given JSON buffer to a POJO of the given class type.
+     * @param buf the JSON buffer.
+     * @param clazz the class to map to.
+     * @param <T> the generic type.
+     * @return an instance of T
+     * @throws DecodeException when there is a parsing or invalid mapping.
+     */
+    public static <T> T decodeValue(Buffer buf, Class<T> clazz) throws DecodeException {
+        try {
+            return mapper.readValue((InputStream) new ByteBufInputStream(buf.getByteBuf()), clazz);
+        } catch (Exception e) {
+            throw new DecodeException("Failed to decode:" + e.getMessage(), e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static Object checkAndCopy(Object val, boolean copy) {
+        if (val == null) {
+            // OK
+        } else if (val instanceof Number && !(val instanceof BigDecimal)) {
+            // OK
+        } else if (val instanceof Boolean) {
+            // OK
+        } else if (val instanceof String) {
+            // OK
+        } else if (val instanceof Character) {
+            // OK
+        } else if (val instanceof CharSequence) {
+            val = val.toString();
+        } else if (val instanceof JsonObject) {
+            if (copy) {
+                val = ((JsonObject) val).copy();
+            }
+        } else if (val instanceof JsonArray) {
+            if (copy) {
+                val = ((JsonArray) val).copy();
+            }
+        } else if (val instanceof Map) {
+            if (copy) {
+                val = (new JsonObject((Map)val)).copy();
+            } else {
+                val = new JsonObject((Map)val);
+            }
+        } else if (val instanceof List) {
+            if (copy) {
+                val = (new JsonArray((List)val)).copy();
+            } else {
+                val = new JsonArray((List)val);
+            }
+        } else if (val instanceof byte[]) {
+            val = Base64.getEncoder().encodeToString((byte[]) val);
+        } else if (val instanceof Instant) {
+            val = ISO_INSTANT.format((Instant) val);
+        } else if (val instanceof Timestamp) {
+            val = MathUtil.getStringFromTimestamp((Timestamp) val);
+        } else if (val instanceof Date) {
+            val = MathUtil.getStringFromDate((java.sql.Date) val);
+        } else {
+            val = val.toString();
+        }
+        return val;
+    }
+
+    static <T> Stream<T> asStream(Iterator<T> sourceIterator) {
+        Iterable<T> iterable = () -> sourceIterator;
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+
+    private static class JsonObjectSerializer extends JsonSerializer<JsonObject> {
+        @Override
+        public void serialize(JsonObject value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeObject(value.getMap());
+        }
+    }
+
+    private static class JsonArraySerializer extends JsonSerializer<JsonArray> {
+        @Override
+        public void serialize(JsonArray value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeObject(value.getList());
+        }
+    }
+
+    private static class InstantSerializer extends JsonSerializer<Instant> {
+        @Override
+        public void serialize(Instant value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeString(ISO_INSTANT.format(value));
+        }
+    }
+
+    private static class ByteArraySerializer extends JsonSerializer<byte[]> {
+        private final Base64.Encoder BASE64 = Base64.getEncoder();
+
+        @Override
+        public void serialize(byte[] value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeString(BASE64.encodeToString(value));
+        }
+    }
+}
