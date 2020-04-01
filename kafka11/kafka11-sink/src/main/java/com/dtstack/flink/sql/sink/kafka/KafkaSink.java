7f28d8bad1acac540aad6b3a7649205ac4830c6f
maqi <maqi@dtstack.com>
2020-04-01 15:26:08 +0800

    kafka sink outtype

--- kafka11/kafka11-sink/src/main/java/com/dtstack/flink/sql/sink/kafka/KafkaSink.java
+++ kafka11/kafka11-sink/src/main/java/com/dtstack/flink/sql/sink/kafka/KafkaSink.java
@@ -49,8 +49,7 @@ public class KafkaSink extends AbstractKafkaSink {
         this.schema = buildTableSchema(fieldNames, fieldTypes);
         this.parallelism = kafka11SinkTableInfo.getParallelism();
         this.sinkOperatorName = SINK_OPERATOR_NAME_TPL.replace("${topic}", topic).replace("${table}", tableName);
-        this.kafkaProducer011 = new KafkaProducer011Factory()
-                .createKafkaProducer(kafka11SinkTableInfo, getOutputType().getTypeAt(1), kafkaProperties, partitioner, partitionKeys);
+        this.kafkaProducer011 = new KafkaProducer011Factory().createKafkaProducer(kafka11SinkTableInfo, getOutputType(), kafkaProperties, partitioner, partitionKeys);
         return this;
     }
 }
