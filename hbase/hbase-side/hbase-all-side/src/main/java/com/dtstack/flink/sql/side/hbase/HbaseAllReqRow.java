a831177893c16154f7d04db625cc4a708be6f907
dapeng <dapeng@dtstack.com>
2020-06-19 16:47:57 +0800

    维表all的类型

--- hbase/hbase-side/hbase-all-side/src/main/java/com/dtstack/flink/sql/side/hbase/HbaseAllReqRow.java
+++ hbase/hbase-side/hbase-all-side/src/main/java/com/dtstack/flink/sql/side/hbase/HbaseAllReqRow.java
@@ -25,6 +25,7 @@ import com.dtstack.flink.sql.side.BaseAllReqRow;
 import com.dtstack.flink.sql.side.FieldInfo;
 import com.dtstack.flink.sql.side.JoinInfo;
 import com.dtstack.flink.sql.side.hbase.table.HbaseSideTableInfo;
+import com.dtstack.flink.sql.side.hbase.utils.HbaseUtils;
 import org.apache.calcite.sql.JoinType;
 import org.apache.commons.collections.map.HashedMap;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
@@ -165,6 +166,7 @@ public class HbaseAllReqRow extends BaseAllReqRow {
 
     private void loadData(Map<String, Map<String, Object>> tmpCache) throws SQLException {
         AbstractSideTableInfo sideTableInfo = sideInfo.getSideTableInfo();
+        Map<String, String> colRefType = ((HbaseAllSideInfo)sideInfo).getColRefType();
         HbaseSideTableInfo hbaseSideTableInfo = (HbaseSideTableInfo) sideTableInfo;
         Configuration conf = new Configuration();
         conf.set("hbase.zookeeper.quorum", hbaseSideTableInfo.getHost());
@@ -177,14 +179,12 @@ public class HbaseAllReqRow extends BaseAllReqRow {
             resultScanner = table.getScanner(new Scan());
             for (Result r : resultScanner) {
                 Map<String, Object> kv = new HashedMap();
-                for (Cell cell : r.listCells())
-                {
+                for (Cell cell : r.listCells()) {
                     String family = Bytes.toString(CellUtil.cloneFamily(cell));
                     String qualifier = Bytes.toString(CellUtil.cloneQualifier(cell));
-                    String value = Bytes.toString(CellUtil.cloneValue(cell));
                     StringBuilder key = new StringBuilder();
                     key.append(family).append(":").append(qualifier);
-
+                    Object value = HbaseUtils.convertByte(CellUtil.cloneValue(cell), colRefType.get(key.toString()));
                     kv.put(aliasNameInversion.get(key.toString()), value);
                 }
                 tmpCache.put(new String(r.getRow()), kv);
