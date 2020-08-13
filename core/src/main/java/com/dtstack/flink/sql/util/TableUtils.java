d417e519815c9528bfbf8c2caed7f62d0b1f676b
dapeng <dapeng@dtstack.com>
2020-08-13 11:42:20 +0800

    修复维表表明没有带有scope的问题

--- core/src/main/java/com/dtstack/flink/sql/util/TableUtils.java
+++ core/src/main/java/com/dtstack/flink/sql/util/TableUtils.java
@@ -26,8 +26,6 @@ import com.google.common.base.Strings;
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import com.typesafe.config.ConfigException;
 import org.apache.calcite.sql.SqlAsOperator;
 import org.apache.calcite.sql.SqlBasicCall;
 import org.apache.calcite.sql.SqlDataTypeSpec;
@@ -49,7 +47,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
-import java.util.stream.Collector;
 
 import static org.apache.calcite.sql.SqlKind.*;
 import static org.apache.calcite.sql.SqlKind.CASE;
@@ -191,25 +188,13 @@ public class TableUtils {
         }
     }
 
-    public static String buildInternalTableName(String left, char split, String right) {
-        StringBuilder sb = new StringBuilder();
-        return sb.append(left).append(split).append(right).toString();
-    }
-
     public static SqlBasicCall buildAsNodeByJoinInfo(JoinInfo joinInfo, SqlNode sqlNode0, String tableAlias) {
         SqlOperator operator = new SqlAsOperator();
 
         SqlParserPos sqlParserPos = new SqlParserPos(0, 0);
         String newTableName = joinInfo.getNewTableName();
-        String lefTbAlias = joinInfo.getLeftTableAlias();
 
-        if(Strings.isNullOrEmpty(lefTbAlias)){
-            Set<String> fromTableSet = Sets.newHashSet();
-            TableUtils.getFromTableInfo(joinInfo.getLeftNode(), fromTableSet);
-            lefTbAlias = StringUtils.join(fromTableSet, "_");
-        }
-
-        String newTableAlias = !StringUtils.isEmpty(tableAlias) ? tableAlias : buildInternalTableName(lefTbAlias, SPLIT, joinInfo.getRightTableAlias());
+        String newTableAlias = !StringUtils.isEmpty(tableAlias) ? tableAlias : joinInfo.getNewTableAlias();
 
         if (null == sqlNode0) {
             sqlNode0 = new SqlIdentifier(newTableName, null, sqlParserPos);
@@ -716,15 +701,15 @@ public class TableUtils {
         //兼容左边表是as 的情况
         String leftStr = Strings.isNullOrEmpty(leftTableName) ? leftTableAlias : leftTableName;
         String newName = leftStr + "_" + rightTableName;
-        String newTableName = TableUtils.buildTableNameWithScope(newName, scope);
         if (CollectionUtils.isEmpty(existTableNames)) {
-            return newName;
+            return TableUtils.buildTableNameWithScope(newName, scope);
         }
-        if (!existTableNames.contains(newName)) {
-            return newName;
-        }
-        return newName + "_" + System.currentTimeMillis();
 
+        if (!existTableNames.contains(newName)) {
+            return TableUtils.buildTableNameWithScope(newName, scope);
+        }
+
+        return TableUtils.buildTableNameWithScope(newName, scope) + "_" + System.currentTimeMillis();
     }
 
 }
