708d9235e40e978da7234a058f705e2cd22e697a
铁柱 <tiezhu@dtstack.com>
2021-01-06 16:08:05 +0800

    [opt-34257][kudu] 优化kudu插件，改用KuduClient API代替AsyncKuduClient

--- kudu/kudu-sink/src/main/java/com/dtstack/flink/sql/sink/kudu/KuduSink.java
+++ kudu/kudu-sink/src/main/java/com/dtstack/flink/sql/sink/kudu/KuduSink.java
@@ -20,28 +20,19 @@ import java.io.Serializable;
 
 public class KuduSink implements RetractStreamTableSink<Row>, Serializable, IStreamSinkGener<KuduSink> {
 
-    private String kuduMasters;
-
-    private String tableName;
-
-    private KuduOutputFormat.WriteMode writeMode;
-
     protected String[] fieldNames;
-
     TypeInformation<?>[] fieldTypes;
-
+    boolean enableKrb;
+    private String kuduMasters;
+    private String tableName;
+    private KuduOutputFormat.WriteMode writeMode;
     private Integer workerCount;
-
     private Integer defaultOperationTimeoutMs;
-
     private Integer defaultSocketReadTimeoutMs;
-
     private int parallelism = -1;
-
     private String principal;
     private String keytab;
     private String krb5conf;
-    boolean enableKrb;
 
     @Override
     public KuduSink genStreamSink(AbstractTargetTableInfo targetTableInfo) {
@@ -67,7 +58,7 @@ public class KuduSink implements RetractStreamTableSink<Row>, Serializable, IStr
 
     public DataStreamSink<Tuple2<Boolean, Row>> consumeDataStream(DataStream<Tuple2<Boolean, Row>> dataStream) {
         KuduOutputFormat.KuduOutputFormatBuilder builder = KuduOutputFormat.buildKuduOutputFormat();
-        builder.setKuduMasters(this.kuduMasters)
+        KuduOutputFormat kuduOutputFormat = builder.setKuduMasters(this.kuduMasters)
                 .setTableName(this.tableName)
                 .setWriteMode(writeMode)
                 .setWorkerCount(this.workerCount)
@@ -78,8 +69,8 @@ public class KuduSink implements RetractStreamTableSink<Row>, Serializable, IStr
                 .setPrincipal(this.principal)
                 .setKeytab(this.keytab)
                 .setKrb5conf(this.krb5conf)
-                .setEnableKrb(this.enableKrb);
-        KuduOutputFormat kuduOutputFormat = builder.finish();
+                .setEnableKrb(this.enableKrb)
+                .finish();
         RichSinkFunction richSinkFunction = new OutputFormatSinkFunction(kuduOutputFormat);
         DataStreamSink dataStreamSink = dataStream.addSink(richSinkFunction);
         dataStreamSink.name(tableName);
